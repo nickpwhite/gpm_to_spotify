56f5c02fdd84f3f4c263f5ca3c5f3e7b5e84649e
Nick White <nick41496@gmail.com>
2017-06-22 18:34:19 -0400

    Add spotify api library

--- gpm_to_spotify.py
+++ gpm_to_spotify.py
@@ -1,38 +1,30 @@
 import json
 import getpass
 import os
+import spotipy
 import sys
 
 from gmusicapi import Mobileclient
-
-def parse_link(gpm_link):
-    raw_title = gpm_link.split('?t=')[1]
-    song_info = raw_title.split('-')
-    title = song_info[0].replace('_', ' ').strip()
-    artist = song_info[1].replace('_', ' ').strip()
-    return title, artist 
 
 username = raw_input("Enter your Google Play Music email address: ")
 password = getpass.getpass("Enter your password: ")
 
 gpm_api = Mobileclient()
+spotify = spotipy.Spotify()
 
-if(not gpm_api.login(username, password, Mobileclient.FROM_MAC_ADDRESS, 'en_US')):
+if (not gpm_api.login(username, password, Mobileclient.FROM_MAC_ADDRESS, 'en_US')):
     print("Unable to login with the provided credentials.")
     sys.exit(-1)
 
-out_file = open('out.txt', 'w')
+out_file = open('out.txt', 'r+')
 songs = gpm_api.get_all_songs()
 
+# dump file for debugging
 json.dump(songs, out_file, indent=4, sort_keys=True)
 
-# if (len(sys.argv) > 1):
-    # for arg in sys.argv[1:]:
-        # print(parse_link(arg))
-# else:
-    # while True:
-        # try:
-            # gpm_link = raw_input("Enter a Google Play Music link (^D to exit): ")
-            # print(parse_link(gpm_link))
-        # except EOFError:
-            # break
+for track in songs[:1]:
+    album = str(track[u'album'])
+    artist = str(track[u'artist'])
+    title = str(track[u'title'])
+    print(album, artist, title)
+    print(spotify.search(q=title, limit=20))
